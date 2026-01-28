17b9fb39bab63242f4349f161f12f923890eae84
Tankun Imboonsu <tankun.imbo@sbac.ac.th>
2026-01-28 15:40:22 +0700

    V2

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 const port = 3005;
 
-app.get('/',(req, res) => {
-    res.send("Mr.Peerapat");
+app.use(express.json());
+
+let drpartment = [
+  { id: 1, name: "IT" },
+  { id: 1, name: "DBT" },
+];
+
+app.get("/", (req, res) => {
+  res.send("Mr.Peerapat");
+});
+
+app.get("/department", (req, res) => {
+  res.send("department");
+});
+
+app.post("/department", (req, res) => {
+  let newdepartment = {
+    id: department.length + 1,
+    name: req.body.name,
+  };
+  department.push(newdepartment);
+  res.send(department);
 });
 
 app.listen(port, () => {
